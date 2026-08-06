7450f301d896949291ad3d078eacddf5cc097f5f
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-08-06 10:19:23 +0300

    refactor: change RequestLoggingAspect

--- task-tracker-backend/src/main/java/task_scheduler/task_tracker_backend/config/RequestLoggingAspect.java
+++ task-tracker-backend/src/main/java/task_scheduler/task_tracker_backend/config/RequestLoggingAspect.java
@@ -1,6 +1,5 @@
 package task_scheduler.task_tracker_backend.config;
 
-import jakarta.servlet.http.HttpSession;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
@@ -9,7 +8,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.CodeSignature;
 import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
 
 @Aspect
 @Component
@@ -21,23 +19,7 @@ public class RequestLoggingAspect {
 
     CodeSignature signature = (CodeSignature) joinPoint.getSignature();
 
-    String[] names = signature.getParameterNames();
-    Object[] values = joinPoint.getArgs();
-
     Map<String, Object> params = new LinkedHashMap<>();
-
-    for (int i = 0; i < names.length; i++) {
-      Object value = values[i];
-
-      if (value instanceof MultipartFile file) {
-        params.put(names[i] + ".name", file.getOriginalFilename());
-        params.put(names[i] + ".size", file.getSize());
-      } else if (value instanceof HttpSession session) {
-        params.put("userId", session.getAttribute("userId"));
-      } else {
-        params.put(names[i], value);
-      }
-    }
 
     log.info(
         "Request: {}.{} {}",
