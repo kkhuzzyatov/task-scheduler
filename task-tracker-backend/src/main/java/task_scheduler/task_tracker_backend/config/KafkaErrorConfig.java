1f4dbc916c33ef839380feb35d9b4fa9688b7a07
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-08-07 16:39:58 +0300

    feat(kafka): add structured kafka error handling

--- task-tracker-backend/src/main/java/task_scheduler/task_tracker_backend/config/KafkaErrorConfig.java
+++ task-tracker-backend/src/main/java/task_scheduler/task_tracker_backend/config/KafkaErrorConfig.java
@@ -1,4 +1,4 @@
-package task_scheduler.task_tracker_email_sender.config;
+package task_scheduler.task_tracker_backend.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
-import task_scheduler.task_tracker_email_sender.properties.LogProperties;
+import task_scheduler.task_tracker_backend.properties.LogProperties;
 
 @RequiredArgsConstructor
 @Slf4j
