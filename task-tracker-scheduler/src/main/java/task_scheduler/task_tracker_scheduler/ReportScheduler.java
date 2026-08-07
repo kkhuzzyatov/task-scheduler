36849a36ac3b78f6470eea7366a72c45c9d4d46c
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-08-07 10:08:54 +0300

    feat(scheduler): add report generation workflow

--- task-tracker-scheduler/src/main/java/task_scheduler/task_tracker_scheduler/ReportScheduler.java
+++ task-tracker-scheduler/src/main/java/task_scheduler/task_tracker_scheduler/ReportScheduler.java
@@ -1,9 +1,26 @@
 package task_scheduler.task_tracker_scheduler;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import task_scheduler.task_tracker_scheduler.service.ReportGenerationService;
 
+@Component
+@RequiredArgsConstructor
 public class ReportScheduler {
 
-  @Scheduled(fixedDelay = 40_000)
-  public void sendReport() {}
+  private final ReportGenerationService reportGenerationService;
+
+  @Scheduled(fixedDelay = 5_000)
+  public void sendReport() {
+    System.out.println("Starting report generation");
+
+    try {
+      reportGenerationService.generateReports();
+      System.out.println("Report generation finished");
+    } catch (Exception e) {
+      System.err.println("Report generation failed");
+      e.printStackTrace();
+    }
+  }
 }
--- task-tracker-scheduler/src/main/java/task_scheduler/task_tracker_scheduler/SchedulerApplication.java
+++ task-tracker-scheduler/src/main/java/task_scheduler/task_tracker_scheduler/SchedulerApplication.java
@@ -2,11 +2,15 @@ package task_scheduler.task_tracker_scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ConfigurationPropertiesScan
 @EnableFeignClients
 @EnableKafka
+@EnableScheduling
 @SpringBootApplication
 public class SchedulerApplication {
 
