eb08bd87846ae2ad02b69285385414128e581aca
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-08-12 21:30:10 +0300

    refactor(scheduler): delegate report generation to backend

--- task-tracker-scheduler/src/main/java/task_scheduler/task_tracker_scheduler/SchedulerApplication.java
+++ task-tracker-scheduler/src/main/java/task_scheduler/task_tracker_scheduler/SchedulerApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ConfigurationPropertiesScan
 @EnableFeignClients
-@EnableKafka
 @EnableScheduling
 @SpringBootApplication
 public class SchedulerApplication {
--- task-tracker-scheduler/src/main/java/task_scheduler/task_tracker_scheduler/client/BackendClient.java
+++ task-tracker-scheduler/src/main/java/task_scheduler/task_tracker_scheduler/client/BackendClient.java
@@ -1,14 +1,12 @@
 package task_scheduler.task_tracker_scheduler.client;
 
-import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import task_scheduler.task_tracker_scheduler.dto.ReportData;
 
 @FeignClient(name = "backend-client", url = "${backend.url}")
 public interface BackendClient {
 
   @GetMapping("/internal/reports")
-  List<ReportData> getReports(@RequestHeader("X-Internal-Api-Key") String apiKey);
+  void sendReports(@RequestHeader("X-Internal-Api-Key") String apiKey);
 }
