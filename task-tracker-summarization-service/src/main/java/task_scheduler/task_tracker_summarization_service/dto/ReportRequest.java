6b13c73a253fa674e6579d10f7d379151e5ba747
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-08-13 13:26:30 +0300

    fix: fix kafka deserialization

--- task-tracker-summarization-service/src/main/java/task_scheduler/task_tracker_summarization_service/dto/ReportRequest.java
+++ task-tracker-summarization-service/src/main/java/task_scheduler/task_tracker_summarization_service/dto/ReportRequest.java
@@ -3,7 +3,9 @@ package task_scheduler.task_tracker_summarization_service.dto;
 import java.util.List;
 import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
+@Jacksonized
 @Value
 @Builder
 public class ReportRequest {
--- task-tracker-summarization-service/src/main/java/task_scheduler/task_tracker_summarization_service/dto/TaskSummaryDto.java
+++ task-tracker-summarization-service/src/main/java/task_scheduler/task_tracker_summarization_service/dto/TaskSummaryDto.java
@@ -2,7 +2,9 @@ package task_scheduler.task_tracker_summarization_service.dto;
 
 import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
+@Jacksonized
 @Value
 @Builder
 public class TaskSummaryDto {
