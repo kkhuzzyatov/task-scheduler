b08736f046205f92d19e95b4da39730422ec150b
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-08-13 13:28:12 +0300

    refactor: exchange generating report path

--- task-tracker-backend/src/main/java/task_scheduler/task_tracker_backend/controller/ReportController.java
+++ task-tracker-backend/src/main/java/task_scheduler/task_tracker_backend/controller/ReportController.java
@@ -6,13 +6,16 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import task_scheduler.task_tracker_backend.dto.report.ReportRequest;
 import task_scheduler.task_tracker_backend.kafka.ReportRequestProducer;
 import task_scheduler.task_tracker_backend.service.AuthService;
 import task_scheduler.task_tracker_backend.service.ReportService;
 import task_scheduler.task_tracker_backend.user.User;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/report")
@@ -31,18 +34,22 @@ public class ReportController {
   private static final String USER_NOT_AUTHENTICATED = "User is not authenticated";
   private static final String INTERNAL_SERVER_ERROR = "Internal server error";
 
-  @Operation(summary = "Get report")
+  @Operation(summary = "Generate report")
   @ApiResponses({
     @ApiResponse(responseCode = STATUS_OK, description = REPORT_RETRIEVED),
     @ApiResponse(responseCode = STATUS_UNAUTHORIZED, description = USER_NOT_AUTHENTICATED),
     @ApiResponse(responseCode = STATUS_INTERNAL_SERVER_ERROR, description = INTERNAL_SERVER_ERROR)
   })
-  @GetMapping
-  public ResponseEntity<Void> getReport(@RequestHeader("Authorization") String token) {
+  @PostMapping("/generate")
+  public ResponseEntity<Void> generateReport(@RequestHeader("Authorization") String token) {
     UUID userId = authService.getMyUuid(token);
     User user = authService.getUserById(userId);
 
-    reportRequestProducer.send(reportService.createReport(user.getEmail()));
+    log.atInfo().log("From token email is " +  user.getEmail());
+
+    ReportRequest reportRequest = reportService.createReport(user.getEmail());
+    log.atInfo().log("In created report request email is " +  reportRequest.getUserEmail());
+    reportRequestProducer.send(reportRequest);
     return ResponseEntity.ok().build();
   }
 }
